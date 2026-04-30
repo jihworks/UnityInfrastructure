3d43b895eabe75196e2cf2975eacbf266e1d1f9f
Jong-il Hong <86797396+jihworks@users.noreply.github.com>
2026-04-30 21:41:22 +0900

    API improved

---
+++ Source/HexaGrid/HexaAnchor.cs
@@ -0,0 +1,147 @@
+﻿// © 2026 Jong-il Hong
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+//
+// SPDX-License-Identifier: MIT
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Jih.Unity.Infrastructure.HexaGrid
+{
+    public readonly struct HexaAnchor
+    {
+        public readonly HexaMap? Map;
+        public readonly HexaCoord Coord;
+
+        public HexaAnchor(HexaCoord coord)
+        {
+            Map = null;
+            Coord = coord;
+        }
+        public HexaAnchor(HexaMap? map, HexaCoord coord)
+        {
+            Map = map;
+            Coord = coord;
+        }
+
+        /// <remarks>
+        /// <see cref="Map"/> required.
+        /// </remarks>
+        public HexaCell? GetNeighbor(HexaNeighborPosition position)
+        {
+            HexaMap map = CheckMap(nameof(GetNeighbor));
+            HexaCoord offset = position.GetOffset();
+            return map.GetCell(Coord + offset);
+        }
+        /// <remarks>
+        /// <see cref="Map"/> required.
+        /// </remarks>
+        public bool TryGetNeighborPosition(HexaCell cell, out HexaNeighborPosition position)
+        {
+            for (int p = 0; p < 6; p++)
+            {
+                HexaNeighborPosition np = (HexaNeighborPosition)p;
+                HexaCell? neighbor = GetNeighbor(np);
+                if (neighbor is not null && neighbor == cell)
+                {
+                    position = np;
+                    return true;
+                }
+            }
+
+            position = default;
+            return false;
+        }
+        /// <remarks>
+        /// <see cref="Map"/> required.
+        /// </remarks>
+        public HexaNeighborPosition GetNeighborPosition(HexaCell cell)
+        {
+            if (!TryGetNeighborPosition(cell, out HexaNeighborPosition result))
+            {
+                throw new InvalidOperationException($"Cell {cell.Coord} is not a neighbor of the cell {Coord}.");
+            }
+            return result;
+        }
+
+        /// <remarks>
+        /// <see cref="Map"/> required.
+        /// </remarks>
+        public HexaCell? GetDiagonal(HexaDiagonalPosition position)
+        {
+            HexaMap map = CheckMap(nameof(GetDiagonal));
+            HexaCoord offset = position.GetOffset();
+            return map.GetCell(Coord + offset);
+        }
+        /// <remarks>
+        /// <see cref="Map"/> required.
+        /// </remarks>
+        public bool TryGetDiagonalPosition(HexaCell cell, out HexaDiagonalPosition position)
+        {
+            for (int p = 0; p < 6; p++)
+            {
+                HexaDiagonalPosition dp = (HexaDiagonalPosition)p;
+                HexaCell? neighbor = GetDiagonal(dp);
+                if (neighbor is not null && neighbor == cell)
+                {
+                    position = dp;
+                    return true;
+                }
+            }
+
+            position = default;
+            return false;
+        }
+        /// <remarks>
+        /// <see cref="Map"/> required.
+        /// </remarks>
+        public HexaDiagonalPosition GetDiagonalPosition(HexaCell cell)
+        {
+            if (!TryGetDiagonalPosition(cell, out HexaDiagonalPosition result))
+            {
+                throw new InvalidOperationException($"Cell {cell.Coord} is not a diagonal of the cell {Coord}.");
+            }
+            return result;
+        }
+
+        /// <remarks>
+        /// <see cref="Map"/> required.
+        /// </remarks>
+        public IEnumerable<HexaCell> EnumerateNeighbors()
+        {
+            for (int p = 0; p < 6; p++)
+            {
+                HexaCell? neighbor = GetNeighbor((HexaNeighborPosition)p);
+                if (neighbor is null)
+                {
+                    continue;
+                }
+                yield return neighbor;
+            }
+        }
+
+        public HexaCoordF GetVertexCoord(HexaVertexPosition position)
+        {
+            HexaCoordF offset = position.GetOffset();
+            return Coord + offset;
+        }
+        public HexaCoordF64 GetVertexCoordF64(HexaVertexPosition position)
+        {
+            HexaCoordF64 offset = position.GetOffsetF64();
+            return Coord + offset;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        HexaMap CheckMap(string context)
+        {
+            if (Map is null)
+            {
+                throw new InvalidOperationException($"Cannot '{context}' without {nameof(Map)} reference.");
+            }
+            return Map;
+        }
+    }
+}
---
+++ Tests/HexaGridTestScript.cs
@@ -0,0 +1,75 @@
+﻿// © 2026 Jong-il Hong
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+//
+// SPDX-License-Identifier: MIT
+
+#nullable enable
+
+using Jih.Unity.Infrastructure;
+using Jih.Unity.Infrastructure.HexaGrid;
+using NUnit.Framework;
+using UnityEngine;
+
+public class HexaGridTestScript
+{
+    [Test]
+    public void VertexCoordAlternativeTest()
+    {
+        HexaMap map = new(128, 128, null, null, null);
+
+        for (int y = 0; y < map.Height; y++)
+        {
+            for (int x = 0; x < map.Width; x++)
+            {
+                HexaCell cell = map[new HexaIndex(x, y)];
+
+                Vector2 cellCenter = o.HexaToScreen(cell.Coord);
+
+                for (int p = 0; p < 6; p++)
+                {
+                    HexaVertexPosition position = (HexaVertexPosition)p;
+
+                    float angle = position.GetRadiusDegrees();
+                    Vector2 radiusVector = MathEx.RadiusVector(angle.ToRadians());
+                    Vector2 centeredLocation = cellCenter + radiusVector;
+
+                    HexaVertex vertex = cell.GetVertex(position);
+                    Vector2 coordedLocation = o.HexaToScreen(vertex.Coord);
+
+                    Assert.IsTrue(centeredLocation.IsNearly(coordedLocation));
+                }
+            }
+        }
+    }
+
+    [Test]
+    public void AnchorCoordAlternativeTest()
+    {
+        HexaMap map = new(128, 128, null, null, null);
+
+        for (int y = 0; y < map.Height; y++)
+        {
+            for (int x = 0; x < map.Width; x++)
+            {
+                HexaIndex index = new(x, y);
+
+                HexaCell cell = map[index];
+                HexaAnchor anchor = new(null, (HexaCoord)index);
+
+                for (int p = 0; p < 6; p++)
+                {
+                    HexaVertexPosition position = (HexaVertexPosition)p;
+
+                    HexaVertex vertex = cell.GetVertex(position);
+                    Vector2 coordedLocation = o.HexaToScreen(vertex.Coord);
+
+                    Vector2 anchoredLocation = o.HexaToScreen(anchor.GetVertexCoord(position));
+
+                    Assert.IsTrue(anchoredLocation.IsNearly(coordedLocation));
+                }
+            }
+        }
+    }
+
+    static readonly HexaOrientation o = new(Vector2.zero, Vector2.one);
+}
