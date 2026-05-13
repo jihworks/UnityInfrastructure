8cbfa3c77b09b90763655ed189811952aee13d65
Jong-il Hong <86797396+jihworks@users.noreply.github.com>
2026-05-13 21:35:57 +0900

    API improved

---
+++ Source/Data/LinkableProperty.cs
@@ -0,0 +1,129 @@
+﻿// © 2026 Jong-il Hong
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+//
+// SPDX-License-Identifier: MIT
+
+#nullable enable
+
+using Jih.Unity.Infrastructure.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Jih.Unity.Infrastructure.Data
+{
+    public class LinkableProperty<T> : IObservable<T>
+    {
+        T _value = default!;
+        public T Value
+        {
+            get
+            {
+                CheckThread();
+                return _value;
+            }
+            set
+            {
+                CheckThread();
+
+                if (EqualityComparer<T>.Default.Equals(value!, _value!))
+                {
+                    return;
+                }
+                _value = value;
+
+                _invokeBuffer.Clear();
+                _invokeBuffer.AddRange(_observers);
+                for (int i = 0; i < _invokeBuffer.Count; i++)
+                {
+                    _invokeBuffer[i].OnNext(value!);
+                }
+                _invokeBuffer.Clear();
+            }
+        }
+
+        readonly List<IObserver<T>> _observers = new();
+        readonly List<IObserver<T>> _invokeBuffer = new();
+
+        readonly int _ownerThreadId;
+
+        public LinkableProperty()
+        {
+            _ownerThreadId = Thread.CurrentThread.ManagedThreadId;
+        }
+
+        IDisposable IObservable<T>.Subscribe(IObserver<T> observer)
+        {
+            CheckThread();
+
+            _observers.Add(observer);
+            observer.OnNext(_value);
+
+            Subscription subscription = _subscriptionPool.Get();
+            subscription.Set(this, observer);
+            return subscription;
+        }
+
+        void Remove(IObserver<T> observer)
+        {
+            CheckThread();
+
+            _observers.Remove(observer);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        void CheckThread()
+        {
+            if (Thread.CurrentThread.ManagedThreadId != _ownerThreadId)
+            {
+                throw new InvalidOperationException("Owner thread mismatched.");
+            }
+        }
+
+        class Subscription : IDisposable
+        {
+            public LinkableProperty<T> Owner { get; private set; } = null!;
+            public IObserver<T> Observer { get; private set; } = null!;
+
+            internal void Set(LinkableProperty<T> owner, IObserver<T> observer)
+            {
+                Owner = owner;
+                Observer = observer;
+            }
+
+            internal void Clear()
+            {
+                Owner = null!;
+                Observer = null!;
+            }
+
+            public void Dispose()
+            {
+                Owner.Remove(Observer);
+
+                _subscriptionPool.Release(this);
+            }
+        }
+
+        class SubscriptionPool : ObjectPool<Subscription>
+        {
+            public SubscriptionPool() : base(isThreadSafe: true)
+            {
+            }
+
+            protected override void Activate(Subscription obj)
+            {
+                base.Activate(obj);
+                obj.Clear();
+            }
+            protected override void Deactivate(Subscription obj)
+            {
+                obj.Clear();
+                base.Deactivate(obj);
+            }
+        }
+
+        static readonly SubscriptionPool _subscriptionPool = new();
+    }
+}
---
+++ Source/Data/ObservableEx.cs
@@ -0,0 +1,230 @@
+﻿// © 2026 Jong-il Hong
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+//
+// SPDX-License-Identifier: MIT
+
+#nullable enable
+
+using System;
+
+namespace Jih.Unity.Infrastructure.Data
+{
+    public static class ObservableEx
+    {
+        public static IObservable<T> Where<T>(this IObservable<T> source, Func<T, bool> predicate)
+        {
+            return new WhereObservable<T>(source, predicate);
+        }
+        public static IObservable<TResult> Select<TSource, TResult>(this IObservable<TSource> source, Func<TSource, TResult> predicate)
+        {
+            return new SelectObservable<TSource, TResult>(source, predicate);
+        }
+
+        public static IObservable<T> Flatten<T>(this IObservable<IObservable<T>> source)
+        {
+            return new FlattenObservable<T>(source);
+        }
+
+        public static IDisposable Link<T>(this IObservable<T> source, Action<T> action)
+        {
+            return source.Subscribe(new ActionObserver<T>(action));
+        }
+
+        public static IObservable<T> Empty<T>()
+        {
+            return EmptyObsavable<T>.Instance;
+        }
+    }
+
+    class FlattenObservable<T> : IObservable<T>
+    {
+        public IObservable<IObservable<T>> Source { get; }
+
+        public FlattenObservable(IObservable<IObservable<T>> source)
+        {
+            Source = source;
+        }
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            return Source.Subscribe(new FlattenObserver<T>(observer));
+        }
+    }
+
+    class FlattenObserver<T> : IObserver<IObservable<T>>
+    {
+        public IObserver<T> Downstream { get; }
+
+        IDisposable? _currentSubscription;
+
+        public FlattenObserver(IObserver<T> downstream)
+        {
+            Downstream = downstream;
+        }
+
+        public void OnNext(IObservable<T> value)
+        {
+            _currentSubscription?.Dispose();
+
+            PassThroughObserver<T> passThroughObserver = new(Downstream);
+            _currentSubscription = value.Subscribe(passThroughObserver);
+        }
+
+        void IObserver<IObservable<T>>.OnCompleted()
+        {
+        }
+        void IObserver<IObservable<T>>.OnError(Exception error)
+        {
+        }
+    }
+
+    class SelectObservable<TSource, TResult> : IObservable<TResult>
+    {
+        public IObservable<TSource> Source { get; }
+        public Func<TSource, TResult> Selector { get; }
+
+        public SelectObservable(IObservable<TSource> source, Func<TSource, TResult> selector)
+        {
+            Source = source;
+            Selector = selector;
+        }
+
+        public IDisposable Subscribe(IObserver<TResult> observer)
+        {
+            SelectObserver<TSource, TResult> selectObserver = new(observer, Selector);
+            return Source.Subscribe(selectObserver);
+        }
+    }
+
+    class SelectObserver<TSource, TResult> : IObserver<TSource>
+    {
+        public IObserver<TResult> Downstream { get; }
+        public Func<TSource, TResult> Selector { get; }
+
+        public SelectObserver(IObserver<TResult> downstream, Func<TSource, TResult> selector)
+        {
+            Downstream = downstream;
+            Selector = selector;
+        }
+
+        public void OnNext(TSource value)
+        {
+            TResult result = Selector(value);
+            Downstream.OnNext(result);
+        }
+
+        void IObserver<TSource>.OnCompleted()
+        {
+        }
+        void IObserver<TSource>.OnError(Exception error)
+        {
+        }
+    }
+
+    class WhereObservable<T> : IObservable<T>
+    {
+        public IObservable<T> Source { get; }
+        public Func<T, bool> Predicate { get; }
+
+        public WhereObservable(IObservable<T> source, Func<T, bool> predicate)
+        {
+            Source = source;
+            Predicate = predicate;
+        }
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            WhereObserver<T> whereObserver = new(observer, Predicate);
+            return Source.Subscribe(whereObserver);
+        }
+    }
+
+    class WhereObserver<T> : IObserver<T>
+    {
+        public IObserver<T> Downstream { get; }
+        public Func<T, bool> Predicate { get; }
+
+        public WhereObserver(IObserver<T> downstream, Func<T, bool> predicate)
+        {
+            Downstream = downstream;
+            Predicate = predicate;
+        }
+
+        public void OnNext(T value)
+        {
+            if (Predicate(value))
+            {
+                Downstream.OnNext(value);
+            }
+        }
+
+        void IObserver<T>.OnCompleted()
+        {
+        }
+        void IObserver<T>.OnError(Exception error)
+        {
+        }
+    }
+
+    class PassThroughObserver<T> : IObserver<T>
+    {
+        public IObserver<T> Downstream { get; }
+
+        public PassThroughObserver(IObserver<T> downstream)
+        {
+            Downstream = downstream;
+        }
+
+        public void OnNext(T value)
+        {
+            Downstream.OnNext(value);
+        }
+
+        void IObserver<T>.OnCompleted()
+        {
+        }
+        void IObserver<T>.OnError(Exception error)
+        {
+        }
+    }
+
+    class ActionObserver<T> : IObserver<T>
+    {
+        public Action<T> Action { get; }
+
+        public ActionObserver(Action<T> action)
+        {
+            Action = action;
+        }
+
+        public void OnNext(T value)
+        {
+            Action(value);
+        }
+
+        void IObserver<T>.OnCompleted()
+        {
+        }
+        void IObserver<T>.OnError(Exception error)
+        {
+        }
+    }
+
+    class EmptyObsavable<T> : IObservable<T>
+    {
+        public static readonly EmptyObsavable<T> Instance = new();
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            return EmptySubscription.Instance;
+        }
+    }
+    class EmptySubscription : IDisposable
+    {
+        public static readonly EmptySubscription Instance = new();
+
+        public void Dispose()
+        {
+        }
+    }
+}
