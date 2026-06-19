4ec771db041dcf0ccf261663553ae1a0fafb01b6
Jong-il Hong <86797396+jihworks@users.noreply.github.com>
2026-06-19 22:26:39 +0900

    API improved

---
+++ Source/UIElements/PointerEventEx.cs
@@ -0,0 +1,26 @@
+﻿// © 2026 Jong-il Hong
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+//
+// SPDX-License-Identifier: MIT
+
+#nullable enable
+
+using UnityEngine.UIElements;
+
+namespace Jih.Unity.Infrastructure.UIElements
+{
+    public static class PointerEventEx
+    {
+        public static UnityMouseButton GetUnityMouseButton<T>(this PointerEventBase<T> e) where T : PointerEventBase<T>, new()
+        {
+            return (UnityMouseButton)e.button;
+        }
+    }
+
+    public enum UnityMouseButton
+    {
+        Left = 0,
+        Right = 1,
+        Middle = 2,
+    }
+}
