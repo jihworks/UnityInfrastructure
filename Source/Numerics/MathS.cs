af1ba24e80714463efcda97dc56e70420a7cd19a
Jong-il Hong <86797396+jihworks@users.noreply.github.com>
2026-06-29 08:54:52 +0900

    API improved

--- Source/Numerics/MathS.cs
+++ Source/Numerics/MathS.cs
@@ -5,10 +5,14 @@
 
 #nullable enable
 
+using System;
+using System.Runtime.CompilerServices;
+
 namespace Jih.Unity.Infrastructure.Numerics
 {
     public static class MathS
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsPointInTriangle(ScreenV t0, ScreenV t1, ScreenV t2, ScreenV p)
         {
             float w1 = ScreenV.Cross(t0, t1, p);
@@ -19,6 +23,31 @@ namespace Jih.Unity.Infrastructure.Numerics
             bool hasPositive = (w1 > Epsilon) || (w2 > Epsilon) || (w3 > Epsilon);
 
             return !(hasNegative && hasPositive);
+        }
+
+        /// <inheritdoc cref="MathEx.GetClosestPointOnLine(UnityEngine.Vector3, UnityEngine.Vector3, UnityEngine.Vector3)"/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ScreenV GetClosestPointOnLine(ScreenV lineStart, ScreenV lineEnd, ScreenV point)
+        {
+            float t = GetClosestPointPositionOnLine(lineStart, lineEnd, point);
+            return lineStart + (lineEnd - lineStart) * t;
+        }
+
+        /// <inheritdoc cref="MathEx.GetClosestPointPositionOnLine(UnityEngine.Vector3, UnityEngine.Vector3, UnityEngine.Vector3)"/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float GetClosestPointPositionOnLine(ScreenV lineStart, ScreenV lineEnd, ScreenV point)
+        {
+            ScreenV lineDirection = lineEnd - lineStart;
+            float lineLengthSq = lineDirection.LengthSquared();
+
+            if (lineLengthSq.IsNearlyZero(tolerance: Epsilon))
+            {
+                return 0f;
+            }
+
+            float dotProduct = ScreenV.Dot(point - lineStart, lineDirection) / lineLengthSq;
+
+            return Math.Clamp(dotProduct, 0f, 1f);
         }
 
         const float Epsilon = 1e-4f;
